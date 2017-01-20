b1416401ef3c9d198fec4c104307542a3a1b27cf
Derrick Or <derrickor@gmail.com>
2017-01-20 14:42:59 -0800

    adjust config options for ui rendering

--- src/main/java/io/confluent/connect/elasticsearch/ElasticsearchSinkConnectorConfig.java
+++ src/main/java/io/confluent/connect/elasticsearch/ElasticsearchSinkConnectorConfig.java
@@ -45,21 +45,25 @@ public class ElasticsearchSinkConnectorConfig extends AbstractConfig {
     final ConfigDef configDef = new ConfigDef();
 
     {
-      final String group = "Connector";
+      final String group = "Connection";
       int order = 0;
       configDef
           .define(CONNECTION_URL_CONFIG, Type.LIST, Importance.HIGH,
                   "List of Elasticsearch HTTP connection URLs e.g. ``http://eshost1:9200,http://eshost2:9200``.",
-                  group, ++order, Width.LONG, "Connection URLs")
+                  group, ++order, Width.NONE, "Connection URLs")
           .define(BATCH_SIZE_CONFIG, Type.INT, 2000, Importance.MEDIUM,
                   "The number of records to process as a batch when writing to Elasticsearch.",
-                  group, ++order, Width.SHORT, "Batch Size")
+                  group, ++order, Width.LONG, "Batch Size")
           .define(MAX_IN_FLIGHT_REQUESTS_CONFIG, Type.INT, 5, Importance.MEDIUM,
                   "The maximum number of indexing requests that can be in-flight to Elasticsearch before blocking further requests.",
-                  group, 5, Width.SHORT, "Max In-flight Requests")
+                  group, 5, Width.LONG, "Max In-flight Requests")
           .define(MAX_BUFFERED_RECORDS_CONFIG, Type.INT, 20000, Importance.LOW,
                   "The maximum number of records each task will buffer before blocking acceptance of more records. This config can be used to limit the memory usage for each task.",
-                  group, ++order, Width.SHORT, "Max Buffered Records")
+                  group, ++order, Width.LONG, "Max Buffered Records")
+          .define(MAX_RETRIES_CONFIG, Type.INT, 5, Importance.LOW,
+                  "The maximum number of retries that are allowed for failed indexing requests. "
+                  + "If the retry attempts are exhausted the task will fail.",
+                  group, ++order, Width.LONG, "Max Retries")
           .define(LINGER_MS_CONFIG, Type.LONG, 1L, Importance.LOW,
                   "Linger time in milliseconds for batching.\n"
                   + "Records that arrive in between request transmissions are batched into a single bulk indexing request, based on the ``" + BATCH_SIZE_CONFIG + "`` configuration. "
@@ -67,20 +71,16 @@ public class ElasticsearchSinkConnectorConfig extends AbstractConfig {
                   + "However it may be desirable to reduce the number of requests even under light load and benefit from bulk indexing. "
                   + "This setting helps accomplish that - when a pending batch is not full, rather than immediately sending it out "
                   + "the task will wait up to the given delay to allow other records to be added so that they can be batched into a single request.",
-                  group, ++order, Width.SHORT, "Linger (ms)")
+                  group, ++order, Width.MEDIUM, "Linger (ms)")
           .define(FLUSH_TIMEOUT_MS_CONFIG, Type.LONG, 10000L, Importance.LOW,
                   "The timeout in milliseconds to use for periodic flushing, "
                   + "and when waiting for buffer space to be made available by completed requests as records are added. "
                   + "If this timeout is exceeded the task will fail.",
-                  group, ++order, Width.SHORT, "Flush Timeout (ms)")
-          .define(MAX_RETRIES_CONFIG, Type.INT, 5, Importance.LOW,
-                  "The maximum number of retries that are allowed for failed indexing requests. "
-                  + "If the retry attempts are exhausted the task will fail.",
-                  group, ++order, Width.SHORT, "Max Retries")
+                  group, ++order, Width.MEDIUM, "Flush Timeout (ms)")
           .define(RETRY_BACKOFF_MS_CONFIG, Type.LONG, 100L, Importance.LOW,
                   "How long to wait in milliseconds before attempting to retry a failed indexing request. "
                   + "This avoids retrying in a tight loop under failure scenarios.",
-                  group, ++order, Width.SHORT, "Retry Backoff (ms)");
+                  group, ++order, Width.MEDIUM, "Retry Backoff (ms)");
     }
 
     {
@@ -89,27 +89,27 @@ public class ElasticsearchSinkConnectorConfig extends AbstractConfig {
       configDef
           .define(TYPE_NAME_CONFIG, Type.STRING, Importance.HIGH,
                   "The Elasticsearch type name to use when indexing.",
-                  group, ++order, Width.SHORT, "Type Name")
+                  group, ++order, Width.NONE, "Type Name")
           .define(KEY_IGNORE_CONFIG, Type.BOOLEAN, false, Importance.HIGH,
                   "Whether to ignore the record key for the purpose of forming the Elasticsearch document ID. "
                   + "When this is set to ``true``, document IDs will be generated as the record's ``topic+partition+offset``.\n"
                   + "Note that this is a global config that applies to all topics, use ``" + TOPIC_KEY_IGNORE_CONFIG + "`` to override as ``true`` for specific topics.",
-                  group, ++order, Width.SHORT, "Ignore Key mode")
+                  group, ++order, Width.LONG, "Ignore Key mode")
           .define(SCHEMA_IGNORE_CONFIG, Type.BOOLEAN, false, Importance.LOW,
                   "Whether to ignore schemas during indexing. "
                   + "When this is set to ``true``, the record schema will be ignored for the purpose of registering an Elasticsearch mapping. "
                   + "Elasticsearch will infer the mapping from the data (dynamic mapping needs to be enabled by the user).\n"
                   + "Note that this is a global config that applies to all topics, use ``" + TOPIC_SCHEMA_IGNORE_CONFIG + "`` to override as ``true`` for specific topics.",
-                  group, ++order, Width.SHORT, "Ignore Schema mode")
+                  group, ++order, Width.LONG, "Ignore Schema mode")
           .define(TOPIC_INDEX_MAP_CONFIG, Type.LIST, "", Importance.LOW,
                   "A map from Kafka topic name to the destination Elasticsearch index, represented as a list of ``topic:index`` pairs.",
-                  group, ++order, Width.LONG, "Topic to Index Map")
+                  group, ++order, Width.NONE, "Topic to Index Map")
           .define(TOPIC_KEY_IGNORE_CONFIG, Type.LIST, "", Importance.LOW,
                   "List of topics for which ``" + KEY_IGNORE_CONFIG + "`` should be ``true``.",
-                  group, ++order, Width.LONG, "Topics for 'Ignore Key' mode")
+                  group, ++order, Width.NONE, "Topics for 'Ignore Key' mode")
           .define(TOPIC_SCHEMA_IGNORE_CONFIG, Type.LIST, "", Importance.LOW,
                   "List of topics for which ``" + SCHEMA_IGNORE_CONFIG + "`` should be ``true``.",
-                  group, ++order, Width.LONG, "Topics for 'Ignore Schema' mode");
+                  group, ++order, Width.NONE, "Topics for 'Ignore Schema' mode");
     }
 
     return configDef;
