e7904e4319fe6dc030e0053d3adebb4c7995e338
Abdelhamide EL ARIB <elarib.abdelhamide@gmail.com>
2018-03-22 17:32:09 +0100

    Add the possibility to use secure es with basic auth

--- src/main/java/io/confluent/connect/elasticsearch/ElasticsearchSinkTask.java
+++ src/main/java/io/confluent/connect/elasticsearch/ElasticsearchSinkTask.java
@@ -116,7 +116,22 @@ public class ElasticsearchSinkTask extends SinkTask {
       if (client != null) {
         this.client = client;
       } else {
-        this.client = new JestElasticsearchClient(props);
+        List<String> address =
+            config.getList(ElasticsearchSinkConnectorConfig.CONNECTION_URL_CONFIG);
+
+        String username =
+                config.getString(ElasticsearchSinkConnectorConfig.CONNECTION_USERNAME_CONFIG);
+        String password =
+                config.getString(ElasticsearchSinkConnectorConfig.CONNECTION_PASSWORD_CONFIG);
+
+        JestClientFactory factory = new JestClientFactory();
+        factory.setHttpClientConfig(
+            new HttpClientConfig.Builder(address)
+                .multiThreaded(true)
+                .defaultCredentials(username, password)
+                .build()
+        );
+        this.client = factory.getObject();
       }
 
       ElasticsearchWriter.Builder builder = new ElasticsearchWriter.Builder(this.client)
