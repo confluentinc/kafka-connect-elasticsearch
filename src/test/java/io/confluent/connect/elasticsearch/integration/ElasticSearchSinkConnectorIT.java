a4d5a07168846b210b90d02c9a089dc19fe1b673
deepesh1234 <pandit.deepesh@hashedin.com>
2020-07-13 19:21:32 +0530

    HCD-329: Improved pom.xml file | removed unwanted dependencies.

--- src/test/java/io/confluent/connect/elasticsearch/integration/ElasticSearchSinkConnectorIT.java
+++ src/test/java/io/confluent/connect/elasticsearch/integration/ElasticSearchSinkConnectorIT.java
@@ -23,7 +23,6 @@ import org.junit.experimental.categories.Category;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
