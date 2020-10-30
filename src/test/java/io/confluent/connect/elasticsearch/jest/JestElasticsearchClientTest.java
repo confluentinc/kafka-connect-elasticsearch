9f5ac6170ab8d9bbfb73a7da8c112f60bfdf6c91
Lev Zemlyanov <lev@confluent.io>
2020-10-30 17:50:01 -0400

    CC-12447: add comprehensive ITs to ES (#454)
    * CC-12447: add comprehensive ITs to ES
    
    Signed-off-by: Lev Zemlyanov <lev@confluent.io>

--- src/test/java/io/confluent/connect/elasticsearch/jest/JestElasticsearchClientTest.java
+++ src/test/java/io/confluent/connect/elasticsearch/jest/JestElasticsearchClientTest.java
@@ -322,7 +322,7 @@ public class JestElasticsearchClientTest {
   @Test
   public void searches() throws Exception {
     JestElasticsearchClient client = new JestElasticsearchClient(jestClient);
-    Search search = new Search.Builder(QUERY).addIndex(INDEX).addType(TYPE).build();
+    Search search = new Search.Builder(QUERY).addIndex(INDEX).addType(TYPE).setParameter("size", 100).build();
     JsonObject queryResult = new JsonObject();
     SearchResult result = new SearchResult(new Gson());
     result.setJsonObject(queryResult);
