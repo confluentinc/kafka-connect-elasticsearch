32b06a2d9aff882f29451cbdb05f2470e3443e58
amuamusshu <35519361+amuamushu@users.noreply.github.com>
2021-05-10 11:49:18 -0700

    Updated ES version in container.

--- src/test/java/io/confluent/connect/elasticsearch/helper/ElasticsearchContainer.java
+++ src/test/java/io/confluent/connect/elasticsearch/helper/ElasticsearchContainer.java
@@ -51,7 +51,7 @@ public class ElasticsearchContainer
   /**
    * Default Elasticsearch version.
    */
-  public static final String DEFAULT_ES_VERSION = "7.0.1";
+  public static final String DEFAULT_ES_VERSION = "7.12.0";
 
   /**
    * Default Elasticsearch port.
