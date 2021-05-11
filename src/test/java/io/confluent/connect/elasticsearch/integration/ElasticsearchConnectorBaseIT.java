1b6a385d5934e8fac8ef85438654978988dc6514
amuamusshu <35519361+amuamushu@users.noreply.github.com>
2021-05-11 13:51:19 -0700

    Update es version to be 7.9.3.

--- src/test/java/io/confluent/connect/elasticsearch/integration/ElasticsearchConnectorBaseIT.java
+++ src/test/java/io/confluent/connect/elasticsearch/integration/ElasticsearchConnectorBaseIT.java
@@ -58,7 +58,6 @@ public class ElasticsearchConnectorBaseIT extends BaseConnectorIT {
   protected Map<String, String> props;
   protected String index;
 
-
   @AfterClass
   public static void cleanupAfterAll() {
     container.close();
