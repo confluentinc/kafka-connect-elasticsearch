3f0f316a6307657aece50f5b4a1626dcedb919eb
amuamusshu <35519361+amuamushu@users.noreply.github.com>
2021-05-11 14:18:36 -0700

    Fix typo in ES version.

--- src/test/java/io/confluent/connect/elasticsearch/helper/ElasticsearchContainer.java
+++ src/test/java/io/confluent/connect/elasticsearch/helper/ElasticsearchContainer.java
@@ -51,7 +51,7 @@ public class ElasticsearchContainer
   /**
    * Default Elasticsearch version.
    */
-  public static final String DEFAULT_ES_VERSION = "7.9.03";
+  public static final String DEFAULT_ES_VERSION = "7.9.3";
 
   /**
    * Default Elasticsearch port.
