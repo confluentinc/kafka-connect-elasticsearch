b388075df864d96be59a6f5e4eaf20a998247348
amuamusshu <35519361+amuamushu@users.noreply.github.com>
2021-05-14 18:09:42 -0700

    Deleted unused import.

--- src/main/java/io/confluent/connect/elasticsearch/DataConverter.java
+++ src/main/java/io/confluent/connect/elasticsearch/DataConverter.java
@@ -16,7 +16,6 @@
 package io.confluent.connect.elasticsearch;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
