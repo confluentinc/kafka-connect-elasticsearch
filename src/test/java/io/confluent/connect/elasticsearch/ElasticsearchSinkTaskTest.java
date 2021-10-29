0fa3bd67d4eae41f14bb43b96b3d05c6304a502c
Alex Diachenko <sansanichfb@gmail.com>
2021-10-28 18:49:42 -0700

    CCMSG-1447: Use an appropriate offset tracker in ElasticsearchClient.

--- src/test/java/io/confluent/connect/elasticsearch/ElasticsearchSinkTaskTest.java
+++ src/test/java/io/confluent/connect/elasticsearch/ElasticsearchSinkTaskTest.java
@@ -27,7 +27,6 @@ import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
