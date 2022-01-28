84cd7e7ca4ca57cdc23f92aa8af7bacd38b9da24
Chris Olivier <cjolivier01@apache.org>
2022-01-28 08:38:17 -0800

    .

--- src/main/java/io/confluent/connect/elasticsearch/ElasticsearchClient.java
+++ src/main/java/io/confluent/connect/elasticsearch/ElasticsearchClient.java
@@ -59,7 +59,7 @@ import org.elasticsearch.client.indices.GetMappingsRequest;
 import org.elasticsearch.client.indices.GetMappingsResponse;
 import org.elasticsearch.client.indices.PutMappingRequest;
 import org.elasticsearch.cluster.metadata.MappingMetadata;
-import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.core.TimeValue;
 import org.elasticsearch.index.VersionType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
