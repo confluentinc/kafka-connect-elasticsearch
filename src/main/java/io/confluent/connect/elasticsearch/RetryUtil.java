a21005831936423da308cb5dc7047ec4df3e7e26
pkuntalteam1 <105704980+pkuntalteam1@users.noreply.github.com>
2022-06-30 12:14:29 +0530

    Adding some more logs to understand the exception.

--- src/main/java/io/confluent/connect/elasticsearch/RetryUtil.java
+++ src/main/java/io/confluent/connect/elasticsearch/RetryUtil.java
@@ -170,6 +170,10 @@ public class RetryUtil {
 
         log.warn("Failed to {} due to {}. Retrying attempt ({}/{}) after backoff of {} ms",
             description, e.getCause(), attempt, maxTotalAttempts, backoff);
+        log.trace("Failed to {} due to {} and stacktrace {}. "
+                         + "Retrying attempt ({}/{}) after backoff of {} ms",
+                         description, e.getCause(), e.getStackTrace(), attempt,
+                         maxTotalAttempts, backoff);
         clock.sleep(backoff);
       }
     }
